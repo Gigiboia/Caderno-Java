7f4419e065ecfd23dbf5a69bbd8d45d577cf0bae
Gigiboia <125405048+Gigiboia@users.noreply.github.com>
2023-06-06 17:44:58 -0300

    Add files via upload

---
+++ main (4).kt
@@ -0,0 +1,15 @@
+fun main(args: Arry<String>) {
+  //criar uma variavel do tipo booleana(falsa true)
+  var nomeVariavel: Boolean = false
+  println(nomeVariavel)
+  var x: Boolean = true
+  println(x)
+  //if faz teste e retona bloolean
+
+  if (x){
+     println("verdadeiro")
+  }
+  else{
+    println("falso")
+  }
+  
